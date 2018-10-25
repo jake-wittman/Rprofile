9bae2e14d2cb92ac5887691a809083551072c402
Jacob T Wittman <wittja01@gmail.com>
2018-10-25 09:06:38 -0500

    script for installing useful R addins

---
+++ install_R_addins.R
@@ -0,0 +1,7 @@
+# Install add ins
+# Mischelper provides a lot of text help functionality for markdown
+devtools::install_github("dracodoc/mischelper", type = "source")
+# Word count and readability statistics
+devtools::install_github("benmarwick/wordcountaddin", type = "source", dependencies = TRUE)
+# Grammar checker in R markdown
+devtools::install_github("ropenscilabs/gramr")
